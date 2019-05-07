38a8b95085ec9d5e4c92d2fc5d39e1dbb5b4714f
mazerunner <23296203+mazerunner70@users.noreply.github.com>
2019-05-07 12:49:40 +0100

    tidies the delivery

--- pycharm/scripts/props.bash
+++ pycharm/scripts/props.bash
@@ -3,3 +3,4 @@
 export pycharm_version_configdir=2019.1
 export pycharm_version_download=2019.1.1
 export docker_tag=pycharm
+#export project_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
