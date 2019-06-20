0e846c7a323528471ef75076dfc8365442ed5233
mazerunner70 <23296203+mazerunner70@users.noreply.github.com>
2019-06-20 22:38:27 +0100

    adds producer to broker
    broker built from  https://jaceklaskowski.gitbooks.io/apache-kafka/kafka-docker.html

---
+++ kafka/nodes/producer/scala/build.sbt
@@ -0,0 +1,9 @@
+name := "scala"
+
+version := "0.1"
+
+//scalaVersion := "2.13.0"
+scalaVersion := "2.12.8"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % "test"
+libraryDependencies += "org.apache.kafka" % "kafka_2.11" % "1.1.1"
---
+++ kafka/nodes/producer/scala/src/main/scala/uk/co/ameth/kafka/producer/Producer.scala
@@ -0,0 +1,47 @@
+package uk.co.ameth.kafka.producer
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+object Producer {
+
+  def main(args: Array[String]): Unit = {
+    val producer = new Producer()
+    producer.run()
+  }
+
+}
+
+class Producer {
+
+  def run(): Unit = {
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("acks", "all")
+    props.put("retries", "0")
+    props.put("batch.size", "16384")
+    props.put("linger.ms", "1")
+    props.put("buffer.memory", "33554432")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    val producer = new KafkaProducer[Long, Long](props)
+
+    val TOPIC = "fibonacci-topic"
+
+    val fibonacci = new Fibonacci()
+    val timeZero = System.currentTimeMillis()
+
+    println("Starting")
+    for( i<- 1 to 5000 * 1000) {
+      val value = fibonacci.fib3(i)
+      print (", "+value)
+      val timestamp = System.currentTimeMillis() - timeZero
+      val producerRecord = new ProducerRecord(TOPIC, timestamp, value)
+      val result = producer.send(producerRecord)
+      println(result)
+      Thread.sleep(100)
+    }
+  }
+
+}
